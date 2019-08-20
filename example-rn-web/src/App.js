2ae87d81fc2ba1b18342c03eac14298a8c3893f7
Adrian Carolli <adrian.carolli1@gmail.com>
2019-08-20 19:13:20 -0400

    Include the library inside the src of the rn-web example

--- example-rn-web/src/App.js
+++ example-rn-web/src/App.js
@@ -8,7 +8,7 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, View } from "react-native-web";
-import { TypingAnimation } from "react-native-typing-animation";
+import TypingAnimation from "./TypingAnimation";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
